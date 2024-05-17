ca35daa7fcbf180745a6c7f96b10414d0f58538b
HJK0000 <queen00220@naver.com>
2024-05-17 17:39:12 +0900

    어노테이션 및 임포트 모두 삭제

--- miniProject/src/main/java/com/green/miniProject/domain/Resume.java
+++ miniProject/src/main/java/com/green/miniProject/domain/Resume.java
@@ -1,23 +1,19 @@
 package com.green.miniProject.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Resume {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private Long rno;
 	private String rintro;
 	private String rimgPath;
--- miniProject/src/main/java/com/green/miniProject/domain/ScoreMemCom.java
+++ miniProject/src/main/java/com/green/miniProject/domain/ScoreMemCom.java
@@ -2,31 +2,19 @@ package com.green.miniProject.domain;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity
 public class ScoreMemCom {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@ToString.Exclude
+
 	private Long smcno;
-	
 	private String mid;
-	
 	private String cno;
 	private int smcscore;
 	private String smcreview;
