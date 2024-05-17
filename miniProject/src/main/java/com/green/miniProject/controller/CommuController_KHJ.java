8a6a9808074fd3f162a60d769a0f9ed3a96a3318
HJK0000 <queen00220@naver.com>
2024-05-17 21:07:48 +0900

    커뮤니티 메인 페이지 생성 및 기능 구현

--- miniProject/src/main/java/com/green/miniProject/controller/CommuController_KHJ.java
+++ miniProject/src/main/java/com/green/miniProject/controller/CommuController_KHJ.java
@@ -1,19 +1,41 @@
 package com.green.miniProject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.green.miniProject.dao.ICommuDao_KHJ;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/commu")
 public class CommuController_KHJ {
-	
+
+	@Autowired
+	private ICommuDao_KHJ dao;
+
 	@RequestMapping("/")
-	public String root() {
-		
-		
+	public String root(Model model) {
+
+		String id = "개인회원";
+		if (id.equals("개인회원")) {
+
+			model.addAttribute("list", dao.getAllWhenMember());
+
+		}
+
 		return "community_KHJ";
 	}
 
-	
-	
+	@RequestMapping("/view")
+	public String view(HttpServletRequest request, Model model) {
+
+		String btitle = request.getParameter("btitle");
+		model.addAttribute("list", dao.detailDao(btitle));
+		System.out.println(model.addAttribute("list", dao.detailDao(btitle)));
+		return "list";
+	}
+
 }
--- miniProject/src/main/java/com/green/miniProject/dao/ICommuDao_KHJ.java
+++ miniProject/src/main/java/com/green/miniProject/dao/ICommuDao_KHJ.java
@@ -1,9 +1,15 @@
 package com.green.miniProject.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.green.miniProject.domain.Board;
 
 @Mapper
 public interface ICommuDao_KHJ {
 	
-	public void get();
+	public List<Board> getAllWhenMember();
+	public Board detailDao(String btitle);
+	
 }
