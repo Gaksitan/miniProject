0fadbbb6c36dd9145fd865bc5e352e46904f6b63
HJK0000 <queen00220@naver.com>
2024-05-17 18:29:39 +0900

    커뮤니티용 컨트롤러 생성

---
+++ miniProject/src/main/java/com/green/miniProject/controller/CommuController_KHJ.java
@@ -0,0 +1,19 @@
+package com.green.miniProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Commu")
+public class CommuController_KHJ {
+	
+	@RequestMapping("/")
+	public String root() {
+		
+		
+		return "";
+	}
+
+	
+	
+}
