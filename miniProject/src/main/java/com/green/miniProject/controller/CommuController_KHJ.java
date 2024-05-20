db7a10abb9343abb249f6a80c36250c051c6e37c
HJK0000 <queen00220@naver.com>
2024-05-20 18:09:15 +0900

    게시글 등록 기능 구현 및 내가쓴 게시글 보기 페이지 생성

--- miniProject/src/main/java/com/green/miniProject/controller/CommuController_KHJ.java
+++ miniProject/src/main/java/com/green/miniProject/controller/CommuController_KHJ.java
@@ -1,17 +1,28 @@
 package com.green.miniProject.controller;
 
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.google.gson.Gson;
 import com.green.miniProject.dao.ICommuDao_KHJ;
 import com.green.miniProject.domain.Board;
+import com.green.miniProject.domain.BoardTag;
 import com.green.miniProject.domain.Member;
+import com.green.miniProject.domain.Tag;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import net.bytebuddy.description.type.TypeVariableToken;
 
 @Controller
 @RequestMapping("/commu")
@@ -24,12 +35,12 @@ public class CommuController_KHJ {
 	public String root(Model model) {
 
 		String id = "개인회원";
-		
+
 		if (id.equals("개인회원")) {
 
 			model.addAttribute("list", dao.getAllWhenMember());
 			System.out.println(model.addAttribute("list", dao.getAllWhenMember()));
-			
+
 		} else if (id.equals("기업회원")) {
 
 			model.addAttribute("list", dao.getAllWhenCompany());
@@ -40,32 +51,88 @@ public class CommuController_KHJ {
 
 	@RequestMapping("/communityMemInsert_KHJ")
 	public String communityMemInsert_KHJ() {
-		
-		
-		
+
 		return "communityMemInsert_KHJ";
 	}
-	
+
 	@RequestMapping("/communityDetail_KHJ")
 	public String communityDetail_KHJ(HttpServletRequest request, Model model) {
-		
-		  String sBno = request.getParameter("bno");
-		  long bno = Long.parseLong(sBno);
-		  List<Object> boardDetail = dao.detailDao(bno);
 
-		  
-		  model.addAttribute("detail", boardDetail);
-		  return "communityDetail_KHJ";
+		String sBno = request.getParameter("bno");
+		long bno = Long.parseLong(sBno);
+		List<BoardTag> boardDetail = dao.detailDao(bno);
+		List<Tag> tagList = new ArrayList<>();
 		
+		for (BoardTag detail : boardDetail) {
+			System.out.println("BoardDetail의 데이터" + detail.getValue());
+			String stag = detail.getValue();
+			
+			Tag tag = new Tag();
+			tag.setValue(stag);
+			tagList.add(tag);
+		}
+		
+		model.addAttribute("boardDetail", boardDetail);
+		model.addAttribute("tagList", tagList);
+		
+			return "communityDetail_KHJ";
+
 	}
-	
-	//@RequestMapping("/view")
-//	public String view(HttpServletRequest request, Model model) {
-//
-//		String btitle = request.getParameter("btitle");
-//		model.addAttribute("list", dao.detailDao(btitle));
-//		System.out.println(model.addAttribute("list", dao.detailDao(btitle)));
-//		return "list";
-//	}
+
+	@RequestMapping("/contentInsert")
+	public String contentInsert(HttpServletRequest request, Model model) {
+
+		String mid = "happy01";
+
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		String tagList = request.getParameter("tag");
+		System.out.println(tagList);
+		dao.insertContentWhenMem(mid, title, content);
+		Long bno = dao.getBno(title, content);
+		Gson gson = new Gson();
+		Type tagListType = new TypeToken<List<Tag>>() {
+		}.getType();
+		System.out.println(tagListType);
+		List<Tag> tags = gson.fromJson(tagList, tagListType);
+		System.out.println(tags);
+		for (Tag tag : tags) {
+			String tagValue = tag.getValue();
+			System.out.println(tagValue);
+			dao.insertTagWhenMem(mid, tagValue, bno);
+		}
+
+			
+			
+
+		
+		
+//				Gson gson = new Gson();
+//				Type tagListType = new TypeToken<List<Tag>>() {
+//				}.getType();
+//				System.out.println(tagListType);
+//				List<Tag> tags = gson.fromJson(tagList, tagListType);
+//				System.out.println(tags);
+//				for (Tag tag : tags) {
+//					String tagValue = tag.getValue();
+//					System.out.println(tagValue);
+//					dao.insertTagWhenMem(mid, tagValue, bno);
+//				
+
+			
+
+		return "redirect:/commu/communityMyBoards_KHJ";
+
+	}
+
+	@RequestMapping("/communityMyBoards_KHJ")
+	public String communityMyBoards_KHJ(Model model, HttpSession session) {
+
+		String mid = "happy01";
+		// Board list = dao.myList(mid);
+		// model.addAttribute("myList", list);
+
+		return "communityMyBoards_KHJ";
+	}
 
 }
--- miniProject/src/main/java/com/green/miniProject/dao/ICommuDao_KHJ.java
+++ miniProject/src/main/java/com/green/miniProject/dao/ICommuDao_KHJ.java
@@ -3,14 +3,19 @@ package com.green.miniProject.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.green.miniProject.domain.Board;
+import com.green.miniProject.domain.BoardTag;
 
 @Mapper
 public interface ICommuDao_KHJ {
 	
 	public List<Board> getAllWhenMember();
 	public List<Board> getAllWhenCompany();
-	public List<Object> detailDao(long bno);
-	
+	public List<BoardTag>detailDao(@Param("bno") long bno);
+	public int insertContentWhenMem(@Param("mid") String mid, @Param("title") String title, @Param("content") String content);
+	public Long getBno(@Param("title") String title, @Param("content") String content);
+	public int insertTagWhenMem(@Param("mid") String mid, @Param("tag") String tag, @Param("bno")Long bno);
+	public Board myList();
 }
